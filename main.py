170e2b4491a42e900138dcaf34c0befaa64cd42a
anthonyclintan007@gmail.com <you@example.com>
2024-01-04 12:21:49 +0530

    Impact Analytics assignment

---
+++ main.py
@@ -0,0 +1,42 @@
+import unittest
+
+class School_combination:
+    def dfs(self, n, path, counts):
+        if len(path) == n:
+            counts[0] += 1
+            if path[-1] == '0':
+                counts[1] += 1
+            return
+        self.dfs(n, path + '1', counts)
+        if path[-3:] != '000':
+            self.dfs(n, path + '0', counts)
+
+    def count_combinations(self, n):
+        #counts[0] for all possibilities, counts[1] probality of missing the Nth day. 
+        counts = [0, 0]
+        self.dfs(n, '1', counts)
+        self.dfs(n, '0', counts)
+
+        ans = str(counts[1])+'/'+str(counts[0])
+        return ans
+
+class TestCombinationCounter(unittest.TestCase):
+    def setUp(self):
+        self.combination_counter = School_combination()
+
+    def test_count_combinations(self):
+        # Test case 1: n = 5
+        ans = self.combination_counter.count_combinations(5)
+        self.assertEqual(ans, '14/29')
+
+        # Test case 2: n = 10
+        ans = self.combination_counter.count_combinations(10)
+        self.assertEqual(ans, '372/773')  
+
+        # Test case 3: n = 3
+        ans = self.combination_counter.count_combinations(3)
+        self.assertEqual(ans, '4/8')  
+
+
+if __name__ == '__main__':
+    unittest.main()
